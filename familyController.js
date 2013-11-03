47a05deb6a03647c799532944ebc6453ed6cc7aa
DeBraid <de.braid@gmail.com>
2013-11-03 11:34:24 -0500

    refactored data into its own script

---
+++ familyController.js
@@ -0,0 +1,9 @@
+function SimpleController($scope) {
+  $scope.family =[
+
+    {name:'derek',  city: 'Burlington', age: '29'}, 
+    {name:'tracy',  city: 'Beamsville', age: '30'}, 
+    {name:'bob',    city: 'Hamilton',   age: '63'}, 
+    {name:'cheryl', city: 'Hamilton',   age: '59'}
+  ];
+}
